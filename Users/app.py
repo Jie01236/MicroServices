32bd7b68eabb8b229f404c733ecd8630508248db
JuniorAwounfouet <juniorawounfouet@gmail.com>
2024-12-11 08:29:52 +0100

    update app.py

--- Users/app.py
+++ Users/app.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
     app = create_app()
     with app.app_context():
         db.create_all()  # Crée la base de données et les tables
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=7000, debug=True)
